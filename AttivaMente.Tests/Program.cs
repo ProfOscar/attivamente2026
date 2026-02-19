c2ebd7b0a4293e9c7c4007759ebaf5b7f92bc697
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-02-19 13:40:49 +0100

    Strutturato menu per applicazione console di test

--- AttivaMente.Tests/Program.cs
+++ AttivaMente.Tests/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿char scelta;
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("--- AttivaMente - Funzionalità di Test ---");
+    Console.WriteLine("\n1) Lista Ruoli");
+    Console.WriteLine("2) Lista Utenti");
+    Console.WriteLine("\nq) ESCI");
+
+    Console.Write("\nScegli la funzione: ");
+    scelta = Console.ReadKey().KeyChar;
+
+    switch (scelta)
+    {
+        case '1':
+            Console.Clear();
+            Console.WriteLine("Hai scelto 1");
+            Console.ReadKey();
+            break;
+        case '2':
+            Console.Clear();
+            Console.WriteLine("Hai scelto 2");
+            Console.ReadKey();
+            break;
+        case 'q':
+        case 'Q':
+            break;
+        default:
+            Console.WriteLine("\n\nScelta non valida, riprova...");
+            Console.ReadKey();
+            break;
+    }
+} while (scelta.ToString().ToLower() != "q");
