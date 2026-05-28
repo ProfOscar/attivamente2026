45aeae76aa2bc93c1160eb67b9f0b48482ae69cc
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-05-28 12:47:05 +0200

    Primi test con Program.cs e MapGet() del progetto Web

--- AttivaMente.Web/Program.cs
+++ AttivaMente.Web/Program.cs
@@ -1,6 +1,20 @@
+using AttivaMente.Core.Models;
+using AttivaMente.Core.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+var utente = new Utente()
+{
+    Nome = "Mario",
+    Cognome = "Rossi",
+    Email = "mario@example.com",
+    PasswordHash = PasswordHelper.HashPassword("MiaPwd50@"),
+    RuoloId = 1
+};
+
+app.MapGet("/", () => "Elenco Utenti:\n" + utente);
+
+app.MapGet("admin/", () => "Admin page (login required)");
 
 app.Run();
