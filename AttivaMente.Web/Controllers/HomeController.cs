f4772a31405d4c5a7fb64e3515f966c64d9c01aa
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2026-05-28 13:39:40 +0200

    Portato la stringa di connessione al DB in appsettings.json e configurato HomeController per poterla utilizzare

--- AttivaMente.Web/Controllers/HomeController.cs
+++ AttivaMente.Web/Controllers/HomeController.cs
@@ -5,9 +5,16 @@ namespace AttivaMente.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            string connStr = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Dati\\AttivaMenteDB.mdf;Integrated Security=True;Connect Timeout=30";
+            string connStr = _configuration.GetConnectionString("DefaultConnection")!;
 
             var utenteRepository = new UtenteRepository(connStr);
             var utenti = utenteRepository.GetAll();
